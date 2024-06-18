289a3f9c8e0b33e811d151be39590c9dc93afb51
Sean B <strawhorse@gmail.com>
2024-06-18 20:24:40 +0100

    Shutdown button and method created

--- src/main/java/com/johnbr/java_spreadsheet_report/Main.java
+++ src/main/java/com/johnbr/java_spreadsheet_report/Main.java
@@ -4,19 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+
+
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+
+
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
+        stage.setTitle("Spreadsheet report generator!");
         stage.setScene(scene);
         stage.show();
     }
 
+
+
+
+
+
+
+//    launch method
     public static void main(String[] args) {
         launch();
     }
