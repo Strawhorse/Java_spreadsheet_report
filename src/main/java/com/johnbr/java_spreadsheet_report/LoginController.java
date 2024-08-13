412352cbe885baca40017a804ad1dbfa39f7c508
Sean B <strawhorse@gmail.com>
2024-08-13 14:28:34 +0100

    cancel button created

--- src/main/java/com/johnbr/java_spreadsheet_report/LoginController.java
+++ src/main/java/com/johnbr/java_spreadsheet_report/LoginController.java
@@ -1,10 +1,25 @@
 package com.johnbr.java_spreadsheet_report;
 
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+
+
 
 public class LoginController {
+
+    @FXML
+    private Button cancelButton;
+
+    public void cancelButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+
+    @FXML
+    private Button loginButton;
 
 
 
