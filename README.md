35fe83d31ba51d7576e7eafa8dd385088825d3fc
Sean B <strawhorse@gmail.com>
2024-08-15 10:59:52 +0100

    Button creation and methods

--- README.md
+++ README.md
@@ -9,6 +9,12 @@ https://openjfx.io/openjfx-docs/
 For connecting to database:
 https://www.youtube.com/watch?v=whhSR0wlWQY&list=PL9CzlNPNQEQ4YCJkl5_4eTH-f-rvF1xps&index=6
 
+Hiding compatibility errors:
+https://stackoverflow.com/questions/35210120/javafx-fxml-api-version-warning
+
+[//]: # (xmlns="http://javafx.com/javafx/22" xmlns:fx="http://javafx.com/fxml/1")
+
+
 
 ![image](https://github.com/user-attachments/assets/c188784a-68f4-4137-80a5-da7fe3a7e809)
 
--- src/main/java/com/johnbr/java_spreadsheet_report/LoginController.java
+++ src/main/java/com/johnbr/java_spreadsheet_report/LoginController.java
@@ -1,9 +1,15 @@
 package com.johnbr.java_spreadsheet_report;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 
@@ -11,16 +17,25 @@ public class LoginController {
 
     @FXML
     private Button cancelButton;
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField enterPasswordField;
+//    methods
+
+
+
 
     public void cancelButtonOnAction(ActionEvent event) {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
 
-
-    @FXML
-    private Button loginButton;
-
+    public void loginButtonOnAction(ActionEvent event) {
+        loginMessageLabel.setText("Login attempted");
+    }
 
 
 
