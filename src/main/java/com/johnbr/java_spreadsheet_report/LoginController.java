b463b4b6ae6d7451458cd0956feef08b9b93ac2b
Sean B <strawhorse@gmail.com>
2024-08-18 11:32:36 +0100

    Fixing method to swap stages between registration and login

--- src/main/java/com/johnbr/java_spreadsheet_report/LoginController.java
+++ src/main/java/com/johnbr/java_spreadsheet_report/LoginController.java
@@ -1,12 +1,18 @@
 package com.johnbr.java_spreadsheet_report;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -19,6 +25,8 @@ public class LoginController {
     @FXML
     private Button cancelButton;
     @FXML
+    private Button registerButton;
+    @FXML
     private Label loginMessageLabel;
     @FXML
     private TextField usernameTextField;
@@ -29,11 +37,6 @@ public class LoginController {
 
 
     //    methods
-
-    public void cancelButtonOnAction(ActionEvent event) {
-        Stage stage = (Stage) cancelButton.getScene().getWindow();
-        stage.close();
-    }
 
     public void loginButtonOnAction(ActionEvent event) {
 
@@ -53,7 +56,7 @@ public class LoginController {
 
 
 //method to validate login against username/password in database
-    public void validateLogin() throws SQLException {
+    public void validateLogin() throws SQLException, IOException {
 
         DatabaseConnection connection = new DatabaseConnection();
         Connection connectToDB = connection.getConnection();
@@ -67,17 +70,29 @@ public class LoginController {
             if(queryResult.getInt(1) == 1) {
                 loginMessageLabel.setText("Login Successful!");
             } else{
-                loginMessageLabel.setText("Login details incorrect. Please try again.");
+//                loginMessageLabel.setText("Login details incorrect. Please try again.");
+                createAccountForm();
             }
         }
 
     }
 
 //    method to swap over to registration form
-    public void createAccountForm() {
+    public void createAccountForm() throws IOException {
+
+        Stage registerstage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Register.fxml"));
+        registerstage.initStyle(StageStyle.UNDECORATED);
+        registerstage.setScene(new Scene(fxmlLoader.load()));
+        registerstage.show();
+    }
 
 
 
+
+    public void cancelButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
     }
 
 
