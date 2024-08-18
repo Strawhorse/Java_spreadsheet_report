bfe1da18af05e26e301a0269071f2a6204648be4
Sean B <strawhorse@gmail.com>
2024-08-18 10:56:03 +0100

    Beginning constants and methods on RegisterController

--- src/main/java/com/johnbr/java_spreadsheet_report/LoginController.java
+++ src/main/java/com/johnbr/java_spreadsheet_report/LoginController.java
@@ -1,20 +1,16 @@
 package com.johnbr.java_spreadsheet_report;
 
-import com.mysql.cj.xdevapi.Result;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ResourceBundle;
 
 
 
--- src/main/java/com/johnbr/java_spreadsheet_report/RegisterController.java
+++ src/main/java/com/johnbr/java_spreadsheet_report/RegisterController.java
@@ -1,11 +1,40 @@
 package com.johnbr.java_spreadsheet_report;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 public class RegisterController {
 
+    @FXML
+    private Button registerButton;
+    @FXML
+    private Button closeButton;
+    @FXML
+    private TextField FirstnameTextField;
+    @FXML
+    private TextField lastnameTextField;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField setPasswordField;
+    @FXML
+    private PasswordField confirmPasswordField;
+
+
+//    methods
+
+    public void cancelButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+
+    
 
 
 }
