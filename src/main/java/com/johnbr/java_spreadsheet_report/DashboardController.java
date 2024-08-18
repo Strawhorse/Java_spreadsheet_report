712cb79ec676564b27ed123b3aece5115a7d1bf3
Sean B <strawhorse@gmail.com>
2024-08-18 17:38:12 +0100

    Creating controller for dashboard

---
+++ src/main/java/com/johnbr/java_spreadsheet_report/DashboardController.java
@@ -0,0 +1,19 @@
+package com.johnbr.java_spreadsheet_report;
+
+public class DashboardController {
+
+
+    
+
+
+
+
+
+
+//    methods
+
+    public void setUsername(String username) {
+//        to pass whichever person is logged in to the dashboard
+    }
+
+}
